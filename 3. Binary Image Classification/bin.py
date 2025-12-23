56eb4ae112c71a05b38d62d1107e06f8268cc6e0
SRIRAM YERRA <132144497+sriram-yerra@users.noreply.github.com>
2025-12-23 12:48:34 +0530

    .

--- 3. Binary Image Classification/bin.py
+++ 3. Binary Image Classification/bin.py
@@ -1,2 +1,14 @@
 from tensorflow.keras import Sequential
-from tensorflow.kera.layers import Conv2D
+from tensorflow.kera.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+# Loading Dataset
+X_train = np.loadtxt('train_input.csv', delimiter = ',')
+Y_train = np.loadtxt('train_labels.csv', delimiter = ',')
+
+X_test = np.loadtxt('test_input.csv', delimiter = ',')
+Y_test = np.loadtxt('test_labels.csv', delimiter = ',')
+
+print(X_train.shape)
+print(Y_train.Y_test)
+print(X_test.Y_test)
+print(Y_test.Y_test)

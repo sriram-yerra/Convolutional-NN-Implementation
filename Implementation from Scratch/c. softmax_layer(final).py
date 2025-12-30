6db16538dd033b2be4a53370cb634f7999975844
SRIRAM YERRA <132144497+sriram-yerra@users.noreply.github.com>
2025-12-30 23:30:36 +0530

    .

--- Implementation from Scratch/c. softmax_layer(final).py
+++ Implementation from Scratch/c. softmax_layer(final).py
@@ -1,5 +1,6 @@
 import numpy as numpy
 
 class Softmax:
-    def __(self, input_len, nodes):
-        
+    def __init__(self, input_len, nodes):
+        self.weights = np.random.randn(input_len, nodes)/input_len
+        self.biases = 

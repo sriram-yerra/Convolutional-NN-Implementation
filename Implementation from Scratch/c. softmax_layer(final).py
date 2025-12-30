9e054ebfce58f31543f67c08939d191360041ff2
SRIRAM YERRA <132144497+sriram-yerra@users.noreply.github.com>
2025-12-31 01:23:22 +0530

    .

--- Implementation from Scratch/c. softmax_layer(final).py
+++ Implementation from Scratch/c. softmax_layer(final).py
@@ -1,6 +1,10 @@
 import numpy as numpy
 
 class Softmax:
+
     def __init__(self, input_len, nodes):
         self.weights = np.random.randn(input_len, nodes)/input_len
-        self.biases = 
+        self.biases = np.zeros(nodes)
+
+    def forward(self, input):
+        # continue after compleling the NNFS Core Implementation..!
